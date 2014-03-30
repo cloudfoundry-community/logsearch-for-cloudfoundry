a3bffd122e4d49d19c6c15970148bfd18fbe5235
David Laing <david@davidlaing.com>
2014-03-30 08:03:41 +0100

    Proper CF log parsing

--- spec/syslog_spec.rb
+++ spec/syslog_spec.rb
@@ -4,7 +4,7 @@ require "logstash/filters/grok"
 describe LogStash::Filters::Grok do
   extend LogStash::RSpec
 
-  describe "JSON app log" do
+  describe "CF syslog/relp" do
 
     config <<-CONFIG
       filter {
@@ -12,29 +12,44 @@ describe LogStash::Filters::Grok do
       }
     CONFIG
 
-    sample '+353 <14>1 2013-12-01T09:41:11+00:00 loggregator 66dcf62e-703d-49d4-b478-319e80f47fd8 App - - {"@timestamp":"2013-12-01T09:41:11.040Z","Name":"Latency CIAPI.PriceStream","Value":"0.085928","@source.name":"pivotal-us-east-1-push1","@source.lonlat":[-78.450065,38.130112],"logger":"Monitors.Common.LatencyMonitor","level":"INFO"}' do
+    sample("message" => '<6>2014-03-29T21:14:35.269841+00:00 10.0.1.13 vcap.hm9000.listener [job=vcap.hm9000.listener index=0] {"timestamp":1396127675.269712210,"process_id":16353,"source":"vcap.hm9000.listener","log_level":"info","message":"Received a heartbeat - {\"Heartbeats Pending Save\":\"1\"}","data":null}', "type" => "relp") do
+
+      insist { subject["type"] } == "relp"
       insist { subject["tags"] }.nil?
-      insist { subject["syslog5424_pri"] } == "14"
-      insist { subject["timestamp"] } == "2013-12-01T09:41"
+      
+      insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:35.269Z").utc
+
+      insist { subject["@shipper.priority"] } == "6"
+      insist { subject["@shipper.host"] } == "10.0.1.13"
+      insist { subject["@shipper.name"] } == "vcap.hm9000.listener"
+      insist { subject["@shipper.job"] } == "vcap.hm9000.listener"
+      insist { subject["@shipper.job_index"] } == "0"
+
+      insist { subject["log_level"] } == "info"
+      insist { subject["message"] } == "Received a heartbeat - {\"Heartbeats Pending Save\":\"1\"}"
+      
     end
 
-    sample '+353 <15>1 2013-12-01T09:41:11+00:00 loggregator 66dcf62e-703d-49d4-b478-319e80f47fd8 App - - {"@timestamp":"2013-12-01T09:41:11.040Z","Name":"Latency CIAPI.PriceStream","Value":"0.085928","@source.name":"pivotal-us-east-1-push1","@source.lonlat":[-78.450065,38.130112],"logger":"Monitors.Common.LatencyMonitor","level":"INFO"}' do
+    sample("message" => '<14>2014-03-29T21:14:33.254640+00:00 10.0.1.13 vcap.nats [job=vcap.nats index=0] {"timestamp":1396127673.254181,"source":"NatsStreamForwarder","log_level":"info","message":"dea.advertise","data":{"nats_message": "{\"id\":\"0-67cf1ac3b917492ab441823225be23d5\",\"stacks\":[\"lucid64\",\"aws-ireland\"],\"available_memory\":2808,\"available_disk\":28928,\"app_id_to_count\":{\"c39a3631-4801-4775-8cfe-fe83d2a41d1f\":1,\"17d96217-9e57-4a78-9c0c-e3394c5d8aa4\":1,\"1d03006f-38b1-4012-a16a-69afc1af8b94\":1},\"placement_properties\":{\"zone\":\"default\"}}","reply_inbox":null}}', "type" => "relp") do
+
+      insist { subject["type"] } == "relp"
       insist { subject["tags"] }.nil?
-      insist { subject["syslog5424_pri"] } == "15"
-      insist { subject["timestamp"] } == "2013-12-01T09:41"
+      
+      insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:33.254Z").utc
+
+      insist { subject["@shipper.priority"] } == "14"
+      insist { subject["@shipper.host"] } == "10.0.1.13"
+      insist { subject["@shipper.name"] } == "vcap.nats"
+      insist { subject["@shipper.job"] } == "vcap.nats"
+      insist { subject["@shipper.job_index"] } == "0"
+
+      insist { subject["log_level"] } == "info"
+      insist { subject["message"] } == "dea.advertise"
+      
     end
 
-    sample '+353 <16>1 2013-12-01T09:41:11+00:00 loggregator 66dcf62e-703d-49d4-b478-319e80f47fd8 App - - {"@timestamp":"2013-12-01T09:41:11.040Z","Name":"Latency CIAPI.PriceStream","Value":"0.085928","@source.name":"pivotal-us-east-1-push1","@source.lonlat":[-78.450065,38.130112],"logger":"Monitors.Common.LatencyMonitor","level":"INFO"}' do
-      insist { subject["tags"] }.nil?
-      insist { subject["syslog5424_pri"] } == "16"
-      insist { subject["timestamp"] } == "2013-12-01T09:41"
-    end
 
-    sample '+353 <17>1 2013-12-01T09:41:11+00:00 loggregator 66dcf62e-703d-49d4-b478-319e80f47fd8 App - - {"@timestamp":"2013-12-01T09:41:11.040Z","Name":"Latency CIAPI.PriceStream","Value":"0.085928","@source.name":"pivotal-us-east-1-push1","@source.lonlat":[-78.450065,38.130112],"logger":"Monitors.Common.LatencyMonitor","level":"INFO"}' do
-      insist { subject["tags"] }.nil?
-      insist { subject["syslog5424_pri"] } == "17"
-      insist { subject["timestamp"] } == "2013-12-01T09:41"
-    end
+
   end
 
 end
