1d64642cbbd9c5a1099c585b9b4251f79a1eda75
David Laing <david@davidlaing.com>
2014-03-30 11:45:48 +0100

    use `@type` and `@message` rather than `type` and `message`

--- spec/syslog_spec.rb
+++ spec/syslog_spec.rb
@@ -8,13 +8,13 @@ describe LogStash::Filters::Grok do
 
     config <<-CONFIG
       filter {
-        #{File.read("target/logstash.filters.cloudfoundry.conf")}
+        #{File.read("target/logstash-filters-cf/logstash-filters-cf.conf")}
       }
     CONFIG
 
-    sample("message" => '<6>2014-03-29T21:14:35.269841+00:00 10.0.1.13 vcap.hm9000.listener [job=vcap.hm9000.listener index=0] {"timestamp":1396127675.269712210,"process_id":16353,"source":"vcap.hm9000.listener","log_level":"info","message":"Received a heartbeat - {\"Heartbeats Pending Save\":\"1\"}","data":null}', "type" => "relp") do
+    sample("@type" => "relp", "@message" => '<6>2014-03-29T21:14:35.269841+00:00 10.0.1.13 vcap.hm9000.listener [job=vcap.hm9000.listener index=0] {"timestamp":1396127675.269712210,"process_id":16353,"source":"vcap.hm9000.listener","log_level":"info","message":"Received a heartbeat - {\"Heartbeats Pending Save\":\"1\"}","data":null}') do
 
-      insist { subject["type"] } == "relp"
+      insist { subject["@type"] } == "cf_relp"
       insist { subject["tags"] }.nil?
       
       insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:35.269Z").utc
@@ -30,9 +30,9 @@ describe LogStash::Filters::Grok do
       
     end
 
-    sample("message" => '<14>2014-03-29T21:14:33.254640+00:00 10.0.1.13 vcap.nats [job=vcap.nats index=0] {"timestamp":1396127673.254181,"source":"NatsStreamForwarder","log_level":"info","message":"dea.advertise","data":{"nats_message": "{\"id\":\"0-67cf1ac3b917492ab441823225be23d5\",\"stacks\":[\"lucid64\",\"aws-ireland\"],\"available_memory\":2808,\"available_disk\":28928,\"app_id_to_count\":{\"c39a3631-4801-4775-8cfe-fe83d2a41d1f\":1,\"17d96217-9e57-4a78-9c0c-e3394c5d8aa4\":1,\"1d03006f-38b1-4012-a16a-69afc1af8b94\":1},\"placement_properties\":{\"zone\":\"default\"}}","reply_inbox":null}}', "type" => "relp") do
+    sample("@type" => "syslog", "@message" => '<14>2014-03-29T21:14:33.254640+00:00 10.0.1.13 vcap.nats [job=vcap.nats index=0] {"timestamp":1396127673.254181,"source":"NatsStreamForwarder","log_level":"info","message":"dea.advertise","data":{"nats_message": "{\"id\":\"0-67cf1ac3b917492ab441823225be23d5\",\"stacks\":[\"lucid64\",\"aws-ireland\"],\"available_memory\":2808,\"available_disk\":28928,\"app_id_to_count\":{\"c39a3631-4801-4775-8cfe-fe83d2a41d1f\":1,\"17d96217-9e57-4a78-9c0c-e3394c5d8aa4\":1,\"1d03006f-38b1-4012-a16a-69afc1af8b94\":1},\"placement_properties\":{\"zone\":\"default\"}}","reply_inbox":null}}') do
 
-      insist { subject["type"] } == "relp"
+      insist { subject["@type"] } == "cf_syslog"
       insist { subject["tags"] }.nil?
       
       insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:33.254Z").utc
