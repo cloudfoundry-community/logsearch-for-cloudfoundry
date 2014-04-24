5e0b9d2de8e6faa8718795a423326f5f536a9e89
Aristoteles Neto <aristoteles.neto@webdrive.co.nz>
2014-04-24 16:52:42 +1200

    Tag parse success and failures with our filter name

--- test/cloudfoundry_spec.rb
+++ test/cloudfoundry_spec.rb
@@ -12,8 +12,7 @@ describe LogStash::Filters::Grok do
 
   describe "Parse Cloud Foundry messages shipped via Syslog or RELP" do
     sample("@type" => "relp", "@message" => '<6>2014-03-29T21:14:35.269841+00:00 10.0.1.13 vcap.hm9000.listener [job=vcap.hm9000.listener index=0] {"timestamp":1396127675.269712210,"process_id":16353,"source":"vcap.hm9000.listener","log_level":"info","message":"Received a heartbeat - {\"Heartbeats Pending Save\":\"1\"}","data":null}') do
-
-      insist { subject["tags"] }.nil?
+      insist { subject["tags"] } == [ 'cloudfoundry' ]
       insist { subject["@type"] } == "relp_cf"
       insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:35.269Z").utc
 
@@ -29,7 +28,7 @@ describe LogStash::Filters::Grok do
     end
 
     sample("@type" => "syslog", "@message" => '<14>2014-03-29T21:14:33.254640+00:00 10.0.1.13 vcap.nats [job=vcap.nats index=0] {"timestamp":1396127673.254181,"source":"NatsStreamForwarder","log_level":"info","message":"dea.advertise","data":{"nats_message": "{\"id\":\"0-67cf1ac3b917492ab441823225be23d5\",\"stacks\":[\"lucid64\",\"aws-ireland\"],\"available_memory\":2808,\"available_disk\":28928,\"app_id_to_count\":{\"c39a3631-4801-4775-8cfe-fe83d2a41d1f\":1,\"17d96217-9e57-4a78-9c0c-e3394c5d8aa4\":1,\"1d03006f-38b1-4012-a16a-69afc1af8b94\":1},\"placement_properties\":{\"zone\":\"default\"}}","reply_inbox":null}}') do
-      insist { subject["tags"] }.nil?
+      insist { subject["tags"] } == [ 'cloudfoundry' ]
       insist { subject["@type"] } == "syslog_cf"
       insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:33.254Z").utc
 
@@ -47,7 +46,7 @@ describe LogStash::Filters::Grok do
 
   describe "Parse NatsStreamForwarder specific messages from Cloud Foundry" do
     sample("@message" => '<14>2014-04-01T06:04:55.213923+00:00 10.0.1.13 vcap.nats [job=vcap.nats index=0]  {"timestamp":1396332295.213669,"source":"NatsStreamForwarder","log_level":"info","message":"router.register","data":{"nats_message": "{\"host\":\"10.0.1.14\",\"port\":9022,\"uris\":[\"api.monitor-cloud.cityindextest5.co.uk\"],\"tags\":{\"component\":\"CloudController\"},\"index\":0,\"private_instance_id\":null}","reply_inbox":null}}', "@type" => "relp") do
-      insist { subject["tags"] }.nil?
+      insist { subject["tags"] } == [ 'cloudfoundry' ]
       
       insist { subject["@job.name"] } == "vcap_nats"
       insist { subject["message"] } == "router.register"
@@ -60,7 +59,7 @@ describe LogStash::Filters::Grok do
 
   describe "Ensure Cloud Foundry messages that fail parsing are tagged appropriately" do
     sample("@type" => "relp", "@message" => '<14>2014-03-29T21:14:33.254640+00:00 10.0.1.13 vcap.nats - this message should fail the CF grok test') do
-      insist { subject["tags"][0]} === "_cfparsefailure"
+      insist { subject["tags"][0] } == "_grokparsefailure-cloudfoundry"
     end  
   end
 
