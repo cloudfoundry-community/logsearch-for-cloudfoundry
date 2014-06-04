2052f0bedf4cadb6f5d2dddf58e214816600b070
Danny Berger <dpb587@gmail.com>
2014-06-04 09:30:27 -0600

    add optional syslog5424_ver to grok /issue #4

--- test/cloudfoundry_spec.rb
+++ test/cloudfoundry_spec.rb
@@ -63,6 +63,24 @@ describe LogStash::Filters::Grok do
       insist { subject["message"] } == "dea.advertise"
     end
 
+    sample("@type" => "syslog", "@message" => '<14>1 2014-03-29T21:14:33.254640+00:00 10.0.1.13 vcap.nats [job=vcap.nats index=0] {"timestamp":1396127673.254181,"source":"NatsStreamForwarder","log_level":"info","message":"dea.advertise","data":{"nats_message": "{\"id\":\"0-67cf1ac3b917492ab441823225be23d5\",\"stacks\":[\"lucid64\",\"aws-ireland\"],\"available_memory\":2808,\"available_disk\":28928,\"app_id_to_count\":{\"c39a3631-4801-4775-8cfe-fe83d2a41d1f\":1,\"17d96217-9e57-4a78-9c0c-e3394c5d8aa4\":1,\"1d03006f-38b1-4012-a16a-69afc1af8b94\":1},\"placement_properties\":{\"zone\":\"default\"}}","reply_inbox":null}}') do
+      insist { subject["tags"] } == [ 'cloudfoundry' ]
+      insist { subject["@type"] } == "syslog_cf"
+      insist { subject["@timestamp"] } == Time.iso8601("2014-03-29T21:14:33.254Z").utc
+
+      insist { subject["@shipper.priority"] } == "14"
+      insist { subject["@shipper.name"] } == "vcap_nats_syslog"
+
+      insist { subject["@job.host"] } == "10.0.1.13"
+      insist { subject["@job.name"] } == "vcap_nats"
+      insist { subject["@job.index"] } == "0"
+
+      insist { subject["log_level"] } == "info"
+      insist { subject["message"] } == "dea.advertise"
+
+      insist { subject["syslog5424_ver"] } == 1
+    end
+
     sample("@type" => "relp", "@message" => '<11>2014-06-03T18:54:10.392723+00:00 10.13.24.115 vcap.cloud_controller_ng.stderr [job=vcap.cloud_controller_ng.stderr index=0]  192.0.2.14, 10.13.17.18, 10.13.24.70 - - [03/Jun/2014 18:54:10] "GET /info HTTP/1.0" 200 283 0.0037') do
       insist { subject["tags"] } == [ 'cloudfoundry' ]
       insist { subject["@type"] } == "relp_cf"
