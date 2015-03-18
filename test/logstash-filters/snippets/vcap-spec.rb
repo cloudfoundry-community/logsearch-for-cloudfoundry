083217219b16c8e0fb08f434bf20467fe59812a2
David Laing <david@davidlaing.com>
2015-03-18 10:16:27 +0000

    Add doppler filters

--- test/logstash-filters/snippets/vcap-spec.rb
+++ test/logstash-filters/snippets/vcap-spec.rb
@@ -9,7 +9,7 @@ describe LogStash::Filters::Grok do
 
   config <<-CONFIG
     filter {
-      #{File.read("vendor/logsearch-filters-common/target/logsearch-filters-default.conf")}
+      #{File.read("vendor/logsearch-boshrelease/logstash-filters-default.conf")} # This simulates the default parsing that logsearch v19+ does
       #{File.read("target/logstash-filters-default.conf")}
     }
   CONFIG
