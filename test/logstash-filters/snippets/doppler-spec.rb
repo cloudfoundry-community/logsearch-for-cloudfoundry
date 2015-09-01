adbd372b39b2737778c367b8ee3229bd0f81a9bf
Simon Johansson <simon@simonjohansson.com>
2015-09-01 10:44:38 +0200

    Dont try to geoip when remote_addr == "-"
    This can happen if you dont use HAProxy as LB,
    https://lists.cloudfoundry.org/archives/list/cf-dev@lists.cloudfoundry.org/thread/NMLZERUP4PCO2OTYRBE4E3MUKSO6ELMD/

--- test/logstash-filters/snippets/doppler-spec.rb
+++ test/logstash-filters/snippets/doppler-spec.rb
@@ -85,7 +85,13 @@ describe LogStash::Filters::Grok do
 
         insist { subject["vcap_request_id"] } == "c66716aa-fef1-482f-55c3-133be3ed8de7"
         insist { subject["response_time"] } == 0.003644458
+      end
 
+      sample("@type" => "syslog", "@message" => '<6>2015-03-17T01:22:43Z jumpbox.xxxxxxx.com doppler[6375]: {"cf_app_id":"ec2d33f6-fd1c-49a5-9a90-031454d1f1ac","level":"info","message_type":"OUT","msg":"cf-env-test.xxxxxxx.com - [17/03/2015:01:21:42 +0000] \"GET / HTTP/1.1\" 200 5087 \"-\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36\" 10.10.0.71:45298 x_forwarded_for:\"-\" vcap_request_id:c66716aa-fef1-482f-55c3-133be3ed8de7 response_time:0.003644458 app_id:ec2d33f6-fd1c-49a5-9a90-031454d1f1ac\n","source_instance":"0","source_type":"RTR","time":"2015-03-17T01:22:43Z"}') do
+        # puts subject.to_hash.to_yaml
+
+        insist { subject["x_forwarded_for"] } == [ "-" ]
+        insist { subject["geoip"] }.nil?
       end
 
       sample("@type" => "syslog", '@message' => '<6>2015-08-17T10:02:18Z canopy-labs.example.com doppler[6375]: {"cf_app_id":"e3c4579a-d3bd-4857-9294-dc6348735848","cf_app_name":"logs","cf_org_id":"c59cb38f-f40a-42b4-ad6c-053413e4b3f3","cf_org_name":"cip-sys","cf_space_id":"637da72a-59ad-4773-987c-72f2d9a53fae","cf_space_name":"elk-for-pcf","event_type":"LogMessage","level":"info","message_type":"OUT","msg":"logs.sys.demo.labs.cf.canopy-cloud.com - [17/08/2015:10:02:17 +0000] \"POST /elasticsearch/_mget?timeout=0\u0026ignore_unavailable=true\u0026preference=1439805736876 HTTP/1.1\" 200 86 352 \"https://logs.sys.demo.labs.cf.canopy-cloud.com/\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:40.0) Gecko/20100101 Firefox/40.0\" 10.0.16.5:35928 x_forwarded_for:\"94.197.120.100\" vcap_request_id:555e9aab-f0bb-49f0-4539-ec257d917435 response_time:0.006633385 app_id:e3c4579a-d3bd-4857-9294-dc6348735848\n","origin":"router__0","source_instance":"0","source_type":"RTR","time":"2015-08-17T10:02:17Z","timestamp":1439805737627585338}') do
