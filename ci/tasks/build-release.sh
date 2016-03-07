c9302656379171afec80c839e96fd40b5792d3b3
Bence Monus <crome@moronia.hu>
2016-03-07 17:48:34 +0000

    remove unused folder
    [#114482869]

--- ci/tasks/build-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-version="$(cat logsearch-for-cloudfoundry-version/number)"
-
-cd logsearch-for-cloudfoundry/src/logsearch-config
-rake build
-
-cd ../..
-bosh create release --force --with-tarball --name logsearch-for-cloudfoundry --version "$version"
--- ci/tasks/test-filters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-cd logsearch-for-cloudfoundry/src/logsearch-config
-
-bin/install-dependencies
-
-export SPEC_OPTS="--format documentation"
-vendor/logstash/vendor/jruby/bin/jruby -S rake test
