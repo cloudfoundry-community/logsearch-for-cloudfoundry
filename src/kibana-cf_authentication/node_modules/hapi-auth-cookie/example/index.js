f04d8ce02ac26b7257da40983c77a3ea55091dc5
Alexey Zakharov <baber@mosga.net>
2016-06-24 17:30:57 +0300

    Revert "Switch to kibana-cf_authentication submodule"

---
+++ src/kibana-cf_authentication/node_modules/hapi-auth-cookie/example/index.js
@@ -0,0 +1,117 @@
+var Hapi = require('hapi');
+
+var uuid = 1;       // Use seq instead of proper unique identifiers for demo only
+
+var users = {
+    john: {
+        id: 'john',
+        password: 'password',
+        name: 'John Doe'
+    }
+};
+
+var home = function (request, reply) {
+
+    reply('<html><head><title>Login page</title></head><body><h3>Welcome '
+      + request.auth.credentials.name
+      + '!</h3><br/><form method="get" action="/logout">'
+      + '<input type="submit" value="Logout">'
+      + '</form></body></html>');
+};
+
+var login = function (request, reply) {
+
+    if (request.auth.isAuthenticated) {
+        return reply.redirect('/');
+    }
+
+    var message = '';
+    var account = null;
+
+    if (request.method === 'post') {
+
+        if (!request.payload.username ||
+            !request.payload.password) {
+
+            message = 'Missing username or password';
+        }
+        else {
+            account = users[request.payload.username];
+            if (!account ||
+                account.password !== request.payload.password) {
+
+                message = 'Invalid username or password';
+            }
+        }
+    }
+
+    if (request.method === 'get' ||
+        message) {
+
+        return reply('<html><head><title>Login page</title></head><body>'
+            + (message ? '<h3>' + message + '</h3><br/>' : '')
+            + '<form method="post" action="/login">'
+            + 'Username: <input type="text" name="username"><br>'
+            + 'Password: <input type="password" name="password"><br/>'
+            + '<input type="submit" value="Login"></form></body></html>');
+    }
+
+    var sid = String(++uuid);
+    request.server.app.cache.set(sid, { account: account }, 0, function (err) {
+
+        if (err) {
+            reply(err);
+        }
+
+        request.auth.session.set({ sid: sid });
+        return reply.redirect('/');
+    });
+};
+
+var logout = function (request, reply) {
+
+    request.auth.session.clear();
+    return reply.redirect('/');
+};
+
+var server = new Hapi.Server();
+server.connection({ port: 8000 });
+
+server.register(require('../'), function (err) {
+
+    var cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 });
+    server.app.cache = cache;
+
+    server.auth.strategy('session', 'cookie', true, {
+        password: 'secret',
+        cookie: 'sid-example',
+        redirectTo: '/login',
+        isSecure: false,
+        validateFunc: function (request, session, callback) {
+
+            cache.get(session.sid, function (err, cached) {
+
+                if (err) {
+                    return callback(err, false);
+                }
+
+                if (!cached) {
+                    return callback(null, false);
+                }
+
+                return callback(null, true, cached.account);
+            });
+        }
+    });
+
+    server.route([
+        { method: 'GET', path: '/', config: { handler: home } },
+        { method: ['GET', 'POST'], path: '/login', config: { handler: login, auth: { mode: 'try' }, plugins: { 'hapi-auth-cookie': { redirectTo: false } } } },
+        { method: 'GET', path: '/logout', config: { handler: logout } }
+    ]);
+
+    server.start(function () {
+
+        console.log('Server ready');
+    });
+});
